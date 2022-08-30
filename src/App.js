c7a372231f28851b21cc42df06abf0dc0bf4657b
faezemohades <mohadesfaeze@gmail.com>
2022-08-30 18:21:50 +0430

    component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChannelDetail from "./components/ChannelDetail";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from "./components/VideoDetail";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+ <BrowserRouter>
+ <Box>
+  <Navbar/>
+  <Routes>
+    <Route path="/" component={<Feed/>}/>
+    <Route path="/video/:id" component={<VideoDetail/>}/>
+    <Route path="/channel/:id" component={<ChannelDetail/>}/>
+    <Route path="/search/:searchTerm" component={<SearchFeed/>}/>
+
+  </Routes>
+ </Box>
+ </BrowserRouter>
   );
 }
 
